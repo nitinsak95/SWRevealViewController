13ad87f093e9279b81a479e6391fc16366ccbc01
affixusimac1 <viveking@outlook.com>
2018-10-10 13:01:13 +0530

    RevealVC

--- SWRevealController/ViewController.swift
+++ SWRevealController/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuButton: UIBarButtonItem!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+      
+        if revealViewController() != nil{
+            menuButton.target = self.revealViewController()
+            menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+            
+            self.view.addGestureRecognizer(revealViewController().panGestureRecognizer())
+        }
     }
 
     override func didReceiveMemoryWarning() {
